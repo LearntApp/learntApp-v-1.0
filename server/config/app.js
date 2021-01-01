6ee4afc5edb62f10811c57520f0696c3d4e1344b
Santiago Yepes <santiagoyepescarrera@gmail.com>
2021-01-01 10:30:56 -0500

    minor changes

--- server/config/app.js
+++ server/config/app.js
@@ -25,7 +25,7 @@ console.log('App Started...');
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // DB Config
-const DB = "mongodb+srv://santiago:OCIImJVgF3Jb2Ii0@mongodbserver.p6m24.mongodb.net/LEARNT?retryWrites=true&w=majority";
+const DB = process.env.MONGO_URI;
 // Connect to Mongo
 mongoose.connect(DB, {useNewUrlParser: true, useUnifiedTopology: true});
 let mongoDB = mongoose.connection;
